7066a291d3d7ad6c5c9745de2839962db3d1f1ff
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-06-08 01:39:01 +0530

    schema/auth-controller/updated-for-reset-password
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -81,6 +81,91 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    if (!user.isEmailVerified) {
+      const existingVerification = await Verification.findOne({
+        userId: user._id,
+      });
+
+      if (existingVerification && existingVerification.expiresAt > new Date()) {
+        return res.status(400).json({
+          message:
+            "Email not verified. Please check your email for verification link.",
+        });
+      } else {
+        await Verification.findByIdAndDelete(existingVerification._id);
+
+        const VerificationToken = jwt.sign(
+          { userId: user._id, purpose: "emailVerification" },
+          process.env.JWT_SECRET,
+          { expiresIn: "1d" }
+        );
+        await Verification.create({
+          userId: user._id,
+          token: VerificationToken,
+          expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        });
+
+        const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}`;
+        const emailBody = `
+      <h1>Welcome to Trelloq</h1>
+      <p>Hi ${name},</p>
+      <p>Thank you for registering with Trelloq. Please click the link below to verify your email address:</p>
+      <a href="${verificationLink}">Verify Email</a>
+      <p>If you did not create an account, please ignore this email.</p>
+    `;
+        const emailSubject = "Verify Your Email - Trelloq";
+
+        const isEmailSent = await sendEmail(email, emailSubject, emailBody);
+
+        if (!isEmailSent) {
+          return res.status(500).json({
+            message:
+              "Failed to send verification email. Please try again later.",
+          });
+        }
+
+        res.status(201).json({
+          message:
+            "Verification email sent successfully to your email. Please check and verify your email to continue",
+        });
+      }
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, purpose: "login" },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    const userData = user.toObject();
+    delete userData.password; // Remove password from user data
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+        isEmailVerified: user.isEmailVerified,
+        profilePicture: user.profilePicture,
+      },
+    });
   } catch (error) {
     console.log(error);
 
@@ -139,4 +224,137 @@ const verifyEmail = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser, verifyEmail };
+const resetPasswordRequest = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!user.isEmailVerified) {
+      return res.status(400).json({
+        message:
+          "Email not verified. Please verify your email before resetting password.",
+      });
+    }
+
+    const existingVerification = await Verification.findOne({
+      userId: user._id,
+    });
+    if (existingVerification && existingVerification.expiresAt > new Date()) {
+      return res.status(400).json({
+        message:
+          "A password reset request is already pending. Please check your email.",
+      });
+    }
+
+    if (existingVerification && existingVerification.expiresAt < new Date()) {
+      await Verification.findByIdAndDelete(existingVerification._id);
+    }
+
+    const resetPasswordToken = jwt.sign(
+      { userId: user._id, purpose: "resetPassword" },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    await Verification.create({
+      userId: user._id,
+      token: resetPasswordToken,
+      expiresAt: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes
+    });
+
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${resetPasswordToken}`;
+    const emailBody = `
+      <h1>Password Reset Request</h1>
+      <p>Hi ${user.name},</p>
+      <p>You requested a password reset. Please click the link below to reset your password:</p>
+      <a href="${resetLink}">Reset Password</a>
+      <p>If you did not request this, please ignore this email.</p>
+    `;
+    const emailSubject = "Reset Your Password - Trelloq";
+
+    const isEmailSent = await sendEmail(email, emailSubject, emailBody);
+
+    if (!isEmailSent) {
+      return res.status(500).json({
+        message: "Failed to send password reset email. Please try again later.",
+      });
+    }
+
+    res.status(200).json({
+      message:
+        "Password reset email sent successfully. Please check your email to reset your password.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const verifyResetPasswordTokenAndResetPassword = async (req, res) => {
+  try {
+    const { token, newPassword, confirmPassword } = req.body;
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    if (!payload) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { userId, purpose } = payload;
+    if (purpose !== "resetPassword") {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const verification = await Verification.findOne({
+      userId,
+      token,
+    });
+    if (!verification) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const isTokenExpired = verification.expiresAt < new Date();
+    if (isTokenExpired) {
+      return res.status(401).json({
+        message: "Token Expired",
+      });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    if (newPassword !== confirmPassword) {
+      return res.status(401).json({
+        message: "Passwords do not match",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).json({ message: "Password Reset Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  verifyEmail,
+  resetPasswordRequest,
+  verifyResetPasswordTokenAndResetPassword,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { validateRequest } from "zod-express-middleware";
 import {
+  emailSchema,
   loginSchema,
   registerSchema,
+  resetPasswordSchema,
   verifyEmailSchema,
 } from "../utils/validateSchema.utils.js";
 import {
   loginUser,
   registerUser,
   verifyEmail,
+  resetPasswordRequest,
+  verifyResetPasswordTokenAndResetPassword,
 } from "../controllers/auth.controller.js";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -33,6 +38,21 @@ router.post(
     body: verifyEmailSchema,
   }),
   verifyEmail
+);
+router.post(
+  "/reset-password-request",
+  validateRequest({
+    body: emailSchema,
+  }),
+  resetPasswordRequest
+);
+
+router.post(
+  "/reset-password",
+  validateRequest({
+    body: resetPasswordSchema,
+  }),
+  verifyResetPasswordTokenAndResetPassword
 );
 
 export { router as authRoutes };
